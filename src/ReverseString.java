a882b74e002b690d6488717e90061307f99314f3
akashghosh <akashghosh.nyc@gmail.com>
2020-01-29 19:39:01 -0500

    Initial commit

---
+++ src/ReverseString.java
@@ -0,0 +1,22 @@
+
+import java.lang.*;
+
+public class ReverseString {
+    
+
+    public static void main(String[] args) {
+
+        StringBuffer rev = new StringBuffer ("HSAKA");
+        System.out.println(rev);
+
+        //reverse the string
+        rev.reverse();
+        System.out.println("String after reversing " + rev);
+
+
+
+
+
+
+    }
+}
